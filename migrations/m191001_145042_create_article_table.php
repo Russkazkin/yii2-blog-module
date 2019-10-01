435edd0013f20b68c547584a8d3fb442e8cac929
Ruslan Skazkopodatelev <ruslan@dkazkin.su>
2019-10-01 18:59:11 +0300

    Complete article migration file

--- migrations/m191001_145042_create_article_table.php
+++ migrations/m191001_145042_create_article_table.php
@@ -3,7 +3,7 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `{{%article}}`.
+ * Handles the creation of table `blog_article`.
  */
 class m191001_145042_create_article_table extends Migration
 {
@@ -12,8 +12,18 @@ class m191001_145042_create_article_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%article}}', [
+        $this->createTable('blog_article', [
             'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'description' => $this->text()->notNull(),
+            'content' => $this->text()->notNull(),
+            'date' => $this->date()->notNull(),
+            'image' => $this->string()->defaultValue('https://via.placeholder.com/300x200.jpg?text=Yii2+Blog+Module+Placeholder'),
+            'viewed' => $this->integer(),
+            'user_id' => $this->integer(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer(11),
+            'updated_at' => $this->integer(11),
         ]);
     }
 
