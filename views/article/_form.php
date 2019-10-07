38cc150f22db1183ea6fce104f1ee8cd7a54c5b4
skazkin <russkazkin@gmail.com>
2019-10-07 13:31:43 +0300

    install datepicker

--- views/article/_form.php
+++ views/article/_form.php
@@ -1,7 +1,11 @@
 <?php
 
+use kartik\date\DatePicker;
+use kartik\icons\FontAwesomeAsset;
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+
+FontAwesomeAsset::register($this);
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\blog\models\Article */
@@ -18,7 +22,13 @@ use yii\bootstrap4\ActiveForm;
 
     <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'date')->textInput() ?>
+    <?= $form->field($model, 'date')->widget(DatePicker::class, [
+        'options' => ['placeholder' => date('Y-m-d')],
+        'pluginOptions' => [
+            'autoclose'=>true,
+            'format' => 'yyyy-mm-dd'
+        ]
+    ]); ?>
 
     <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
 
