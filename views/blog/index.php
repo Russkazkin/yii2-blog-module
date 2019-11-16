f3bf6b6a46cd7ed6bc056f7ca7b2602e469256af
skazkin <russkazkin@gmail.com>
2019-11-16 15:04:39 +0300

    Add article loop to view

--- views/blog/index.php
+++ views/blog/index.php
@@ -12,7 +12,7 @@ use yii\bootstrap4\LinkPager; ?>
     <?php foreach ($models as $model): ?>
         <article class="post">
             <div class="post-thumb">
-                <a href="blog.html"><img src="/temp/blog-1.jpg" alt=""></a>
+                <a href="blog.html"><img src="<?= $model->getImage(); ?>" alt="<?= $model->image; ?>"></a>
 
                 <a href="blog.html" class="post-thumb-overlay text-center">
                     <div class="text-uppercase text-center">View Post</div>
@@ -20,21 +20,14 @@ use yii\bootstrap4\LinkPager; ?>
             </div>
             <div class="post-content">
                 <header class="entry-header text-center text-uppercase">
-                    <h6><a href="#"> Travel</a></h6>
+                    <h6><a href="#"><?= $model->category->title; ?></a></h6>
 
-                    <h1 class="entry-title"><a href="blog.html">Home is peaceful place</a></h1>
+                    <h1 class="entry-title"><a href="blog.html"><?= $model->title; ?></a></h1>
 
 
                 </header>
                 <div class="entry-content">
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                        tevidulabore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-                        accusam et
-                        justo duo dolores rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                        Lorem
-                        ipsum dolor sit am Lorem ipsum dolor sitconsetetur sadipscing elitr, sed diam nonumy
-                        eirmod tempor invidunt ut labore et dolore maliquyam erat, sed diam voluptua.
-                    </p>
+                    <p><?= $model->description; ?></p>
 
                     <div class="btn-continue-reading text-center text-uppercase">
                         <a href="blog.html" class="more-link">Continue Reading</a>
@@ -42,10 +35,10 @@ use yii\bootstrap4\LinkPager; ?>
                 </div>
                 <div class="social-share">
                             <span class="social-share-title float-left text-capitalize">By <a href="#">Rubel</a> On
-                                February 12, 2016</span>
+                                <?= $model->date; ?></span>
                     <ul class="text-center float-right">
                         <li><a class="s-facebook" href="#"><i class="fas fa-eye"></i></a></li>
-                        325
+                        <?= (int) $model->viewed; ?>
                     </ul>
                 </div>
             </div>
