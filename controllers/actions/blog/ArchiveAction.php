21528e67367aceccdd4cd4433f97afb9da50e757
skazkin <russkazkin@gmail.com>
2019-11-26 14:17:51 +0300

    Add dinamic category list page

--- controllers/actions/blog/ArchiveAction.php
+++ controllers/actions/blog/ArchiveAction.php
@@ -4,13 +4,29 @@
 namespace app\modules\blog\controllers\actions\blog;
 
 
-use app\modules\blog\controllers\actions\BaseAction;
+use app\modules\blog\models\Article;
+use yii\data\Pagination;
 
 class ArchiveAction extends BaseBlogAction
 {
-    public function run()
+    public function run($category_id = null, $tag_id = null)
     {
+        if(isset($category_id)) {
+            $query = Article::find()
+                ->andWhere(['category_id' => $category_id])
+                ->andWhere(['status' => 10])
+                ->orderBy(['date' => SORT_DESC]);
+        } elseif (isset($tag_id)) {
+            echo 'it\'s tag';
+        }
+        $countQuery = clone $query;
+        $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => 2]);
+        $models = $query->offset($pages->offset)
+            ->limit($pages->limit)
+            ->all();
         return $this->controller->render('archive', [
+            'models' => $models,
+            'pages' => $pages,
             'dateManager' => $this->controller->dateManager,
             'sidebarData' => $this->getSidebarData(),
         ]);
