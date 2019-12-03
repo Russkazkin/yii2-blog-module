c7aff9ed5698356cc4547bc9aa95ba4aa767db20
Ruslan Skazkopodatelev <ruslan@skazkin.su>
2019-12-03 12:43:35 +0300

    Optimize getSidebarData() method

--- controllers/actions/blog/BaseBlogAction.php
+++ controllers/actions/blog/BaseBlogAction.php
@@ -11,27 +11,12 @@ use yii\db\ActiveQuery;
 
 class BaseBlogAction extends BaseAction
 {
-    private function getCategoriesList()
-    {
-        return Category::find()->all();
-    }
-
-    private function getPopularArticles()
-    {
-        return Article::find()->orderBy('viewed desc')->limit(3)->all();
-    }
-
-    private function getRecentArticles()
-    {
-        return Article::find()->orderBy('date desc')->limit(4)->all();
-    }
-
     protected function getSidebarData()
     {
         $data = [];
-        $data['popular'] = self::getPopularArticles();
-        $data['recent'] = self::getRecentArticles();
-        $data['categories'] = self::getCategoriesList();
+        $data['popular'] = Article::find()->orderBy('viewed desc')->limit(3)->all();
+        $data['recent'] = Article::find()->orderBy('date desc')->limit(4)->all();
+        $data['categories'] = Category::find()->all();
         return $data;
     }
 
