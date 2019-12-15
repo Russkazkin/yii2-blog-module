5236f171b394fdf31e70d8eebcdb30596f839456
skazkin <russkazkin@gmail.com>
2019-12-15 16:06:30 +0300

    Remove grid widget from index view

--- views/article/index.php
+++ views/article/index.php
@@ -14,85 +14,34 @@ $this->title = Yii::t('app', 'Articles');
 $this->params['breadcrumbs'][] = $this->title;
 DataTablesAsset::register($this);
 ?>
-<div class="article-index">
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Article'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php  // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        //'tableOptions' => ['class' => ]
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            //'id',
-            'title',
-            [
-                'label' => Yii::t('app', 'Category'),
-                'attribute' => 'category_id',
-                'value' => function($data) {
-                    return $data->category->title;
-                }
-            ],
-            [
-                'label' => Yii::t('app', 'Tags'),
-                'value' => function($data) {
-                    return $data->selectedTagsTitle;
-                }
-            ],
-            'description:ntext',
-            //'content:ntext',
-            'date:date',
-            [
-                'attribute' => 'image',
-                'format' => 'html',
-                'label' => Yii::t('app', 'Image'),
-                'value' => function($data){
-                    return Html::img($data->getImage(), ['height' => '50']);
-                },
-            ],
-            //'viewed',
-            //'user_id',
-            //'status',
-            //'created_at',
-            //'updated_at',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-</div>
-<?php //var_dump($articles); ?>
 <div class="row">
-        <div class="col-12">
-            <div class="card-box">
-                <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap">
-                    <thead>
-                    <tr>
-                        <th>Actions</th>
-                        <th>Title</th>
-                        <th>Category</th>
-                        <th>Date</th>
-                        <th>Tags</th>
-                        <th>Image</th>
-
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($articles as $article):?>
-                    <tr>
-                        <td></td>
-                        <td><?= $article->title; ?></td>
-                        <td><?= $article->category->title; ?></td>
-                        <td><?= $article->date; ?></td>
-                        <td><?= $article->tags; ?></td>
-                        <td><?= $article->getImage(); ?></td>
-                    </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
+    <div class="col-12">
+        <div class="card-box">
+            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap">
+                <thead>
+                <tr>
+                    <th>Actions</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                    <th>Date</th>
+                    <th>Tags</th>
+                    <th>Image</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($articles as $article):?>
+                <tr>
+                    <td></td>
+                    <td><?= $article->title; ?></td>
+                    <td><?= $article->category->title; ?></td>
+                    <td><?= $article->date; ?></td>
+                    <td><?= $article->tags; ?></td>
+                    <td><?= $article->getImage(); ?></td>
+                </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
+    </div>
 </div>
