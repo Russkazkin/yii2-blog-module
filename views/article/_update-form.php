6dbb6479a79dbc9d76370176057f5d17ee41f4c3
Ruslan Skazkopodatelev <ruslan@skazkin.su>
2019-12-29 19:23:33 +0300

    Update update article view with adminto theme

--- views/article/_update-form.php
+++ views/article/_update-form.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\modules\blog\models\Article;
+use bizley\quill\Quill;
 use kartik\date\DatePicker;
 use kartik\file\FileInput;
 use kartik\icons\FontAwesomeAsset;
@@ -21,56 +22,100 @@ FontAwesomeAsset::register($this);
 ?>
 
 <div class="article-form">
+    <div class="col-12">
+        <div class="p-2">
 
-    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+            <?php $form = ActiveForm::begin([
+                'layout'=>'horizontal',
+                'options' => ['enctype' => 'multipart/form-data', 'class' => 'form-horizontal'],
+                'fieldConfig' => [
+                    'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+                    'horizontalCssClasses' => [
+                        'label' => 'col-sm-2 col-form-label',
+                        'wrapper' => 'col-sm-10',
+                        'error' => '',
+                        'hint' => '',
+                    ],
+                ],
+            ]); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'category_id')->dropDownList($categories,
-        ['prompt' => Yii::t('app', 'Choose category...')]); ?>
+            <?= $form->field($model, 'category_id')->dropDownList($categories,
+                ['prompt' => Yii::t('app', 'Choose category...')]); ?>
 
-    <?= Html::label('Tags', 'tags', ['class' => 'label tags']) ?>
-    <?= Html::dropDownList('tags', $selectedTags, $tags, ['class' => 'form-control', 'multiple' => true]) ?>
+            <div class="row form-group">
+                <?= Html::label('Tags', 'tags', ['class' => 'label tags col-sm-2 col-form-label']) ?>
+                <div class="col-sm-10">
+                    <?= Html::dropDownList(
+                        'tags',
+                        $selectedTags,
+                        $tags,
+                        ['id' => 'article-tags', 'class' => 'form-control', 'multiple' => true])
+                    ?>
+                </div>
+            </div>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+            <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+            <?= $form->field($model, 'content')->widget(Quill::class, [
+                'theme' => 'snow',
+                'toolbarOptions' => [
+                    [['font' => []], ['size' => []]],
+                    ['bold', 'italic', 'underline', 'strike'],
+                    [['color' => []], ['background' => []]],
+                    [['script' => 'super'], ['script' => 'sub']],
+                    [['header' => [!1, 1, 2, 3, 4, 5, 6]], 'blockquote', 'code-block'],
+                    [['list' => 'ordered'], ['list' => 'bullet'], ['indent' => '-1'], ['indent' => '+1']],
+                    ['direction', ['align' => []]],
+                    ['link', 'image', 'video', 'formula'],
+                    ['clean'],
+                ],
+                'options' => [
+                    'style' => 'min-height:300px;'
+                ],
+                'modules' => [
+                    'formula' => true,
+                    'syntax' => true,
+                ]
+            ]) ?>
 
-    <?= $form->field($model, 'date')->widget(DatePicker::class, [
-        'options' => ['value' => $today],
-        'pluginOptions' => [
-            'autoclose' => true,
-            'format' => $model->format,
-        ]
-    ]); ?>
+            <?= $form->field($model, 'date')->widget(DatePicker::class, [
+                'options' => ['value' => $today],
+                'pluginOptions' => [
+                    'autoclose' => true,
+                    'format' => $model->format,
+                ]
+            ]); ?>
 
-    <?= $form->field($model, 'file')->widget(FileInput::class, [
-        'language' => 'ru',
-        'options' => [
-            'accept' => 'image/*',
+            <?= $form->field($model, 'file')->widget(FileInput::class, [
+                'language' => 'ru',
+                'options' => [
+                    'accept' => 'image/*',
 
-        ],
-        'pluginOptions' => [
-            'initialPreviewAsData' => true,
-            'initialPreview' => [
-                $imagePreview,
-            ],
-            'overwriteInitial' => true,
-            'deleteUrl' => '/blog/article/remove-img',
-            'initialPreviewConfig' => [
-                ['caption' => $model->image, 'key' => $model->id],
-            ],
-            'showUpload' => false,
-            'showClose' => false,
-        ],
+                ],
+                'pluginOptions' => [
+                    'initialPreviewAsData' => true,
+                    'initialPreview' => [
+                        $imagePreview,
+                    ],
+                    'overwriteInitial' => true,
+                    'deleteUrl' => '/blog/article/remove-img',
+                    'initialPreviewConfig' => [
+                        ['caption' => $model->image, 'key' => $model->id],
+                    ],
+                    'showUpload' => false,
+                    'showClose' => false,
+                ],
 
-    ]); ?>
+            ]); ?>
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
 
--- views/article/update.php
+++ views/article/update.php
@@ -1,29 +1,32 @@
 <?php
 
+use app\modules\admin\assets\FormAdvancedAsset;
+use app\modules\blog\Module;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\blog\models\Article */
 /* @var $imagePreview*/
 
-$this->title = Yii::t('app', 'Update Article: {name}', [
+$this->title = Module::t('blog', 'Update Article: {name}', [
     'name' => $model->title,
 ]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Articles'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+FormAdvancedAsset::register($this);
 ?>
-<div class="article-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_update-form', [
-        'model' => $model,
-        'today' => date($model->phpFormat, $model->date),
-        'imagePreview' => $imagePreview,
-        'categories' => $model->categoriesList,
-        'tags' => $model->tagsList,
-        'selectedTags' => $model->selectedTags,
-    ]) ?>
-
+<div class="row article-update">
+    <div class="col-12">
+        <div class="card-box">
+            <?= $this->render('_update-form', [
+                'model' => $model,
+                'today' => date($model->phpFormat, $model->date),
+                'imagePreview' => $imagePreview,
+                'categories' => $model->categoriesList,
+                'tags' => $model->tagsList,
+                'selectedTags' => $model->selectedTags,
+            ]) ?>
+        </div>
+    </div>
 </div>
