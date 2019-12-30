86fca7637ba64bd8b26b59218bf1ecf320c8b0db
Ruslan Skazkopodatelev <ruslan@skazkin.su>
2019-12-30 17:48:17 +0300

    Add rbac check for article view action

--- controllers/actions/article/ViewAction.php
+++ controllers/actions/article/ViewAction.php
@@ -5,13 +5,19 @@ namespace app\modules\blog\controllers\actions\article;
 
 
 use app\modules\blog\controllers\actions\BaseAction;
+use yii\web\UnauthorizedHttpException;
 
 class ViewAction extends BaseAction
 {
     public function run($id)
     {
+        $model = $this->controller->findModel($id);
+
+        if (!$this->controller->rbacManager->canViewArticle($model)){
+            throw new UnauthorizedHttpException('You can\'t view this article');
+        }
         return $this->controller->render('view', [
-            'model' => $this->controller->findModel($id),
+            'model' => $model,
             'articleComponent' => $this->controller->articleComponent,
             'rbacManager' => $this->controller->rbacManager,
         ]);
