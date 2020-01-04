7c406d9eb6f1ffd968adeb232f0f004c843ef00a
skazkin <russkazkin@gmail.com>
2020-01-04 10:09:02 +0300

    Update category create view

--- views/category/create.php
+++ views/category/create.php
@@ -1,6 +1,6 @@
 <?php
 
-use yii\helpers\Html;
+use app\modules\admin\assets\FormAdvancedAsset;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\blog\models\Category */
@@ -8,13 +8,14 @@ use yii\helpers\Html;
 $this->title = Yii::t('blog', 'Create Category');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('blog', 'Categories'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+FormAdvancedAsset::register($this);
 ?>
-<div class="category-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
+<div class="category-create row">
+    <div class="col-12">
+        <div class="card-box">
+            <?= $this->render('_form', [
+                'model' => $model,
+            ]) ?>
+        </div>
+    </div>
 </div>
