fb6f7a8dbea76ae8d744212f2098465d78d974e4
skazkin <russkazkin@gmail.com>
2020-01-06 14:33:43 +0300

    Add minor fixes

--- controllers/actions/category/DeleteAction.php
+++ controllers/actions/category/DeleteAction.php
@@ -5,13 +5,13 @@ namespace app\modules\blog\controllers\actions\category;
 
 
 use app\modules\blog\controllers\actions\BaseAction;
-use Yii;
 
 class DeleteAction extends BaseAction
 {
     public function run($id)
     {
         $this->controller->findModel($id)->delete();
+
         return $this->controller->redirect(['index']);
     }
 }
