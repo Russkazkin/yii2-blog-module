7fe7d0adbf97e2e40e14fd7a7fd056ce2186eda3
skazkin <russkazkin@gmail.com>
2020-01-07 08:34:13 +0300

    Close category deletion for users

--- controllers/actions/category/DeleteAction.php
+++ controllers/actions/category/DeleteAction.php
@@ -5,9 +5,19 @@ namespace app\modules\blog\controllers\actions\category;
 
 
 use app\modules\blog\controllers\actions\BaseAction;
+use app\modules\blog\Module;
+use yii\web\UnauthorizedHttpException;
 
 class DeleteAction extends BaseAction
 {
+    public function beforeRun()
+    {
+        if(!$this->controller->rbacManager->haveAdminPermissions()){
+            throw new UnauthorizedHttpException(Module::t('blog', 'Unauthorized Access'));
+        }
+        return parent::beforeRun();
+    }
+
     public function run($id)
     {
         $this->controller->findModel($id)->delete();
