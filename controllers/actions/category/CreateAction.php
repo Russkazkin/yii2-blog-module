5c75351ca562d769b97ba15463a905570a11ff0f
skazkin <russkazkin@gmail.com>
2020-01-07 08:31:22 +0300

    Close category creation for users

--- controllers/actions/category/CreateAction.php
+++ controllers/actions/category/CreateAction.php
@@ -8,9 +8,18 @@ use app\modules\blog\controllers\actions\BaseAction;
 use app\modules\blog\models\Category;
 use app\modules\blog\Module;
 use Yii;
+use yii\web\UnauthorizedHttpException;
 
 class CreateAction extends BaseAction
 {
+    public function beforeRun()
+    {
+        if(!$this->controller->rbacManager->haveEditorPermissions()){
+            throw new UnauthorizedHttpException(Module::t('blog', 'Unauthorized Access'));
+        }
+        return parent::beforeRun();
+    }
+
     public function run()
     {
         $model = new Category();
