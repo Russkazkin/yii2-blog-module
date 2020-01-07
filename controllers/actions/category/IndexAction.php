8eb855b8d1b6bf95350a71f374206da4b688f3b3
skazkin <russkazkin@gmail.com>
2020-01-07 08:37:17 +0300

    Close categories list for users

--- controllers/actions/category/IndexAction.php
+++ controllers/actions/category/IndexAction.php
@@ -8,9 +8,18 @@ use app\modules\blog\controllers\actions\BaseAction;
 use app\modules\blog\models\Category;
 use app\modules\blog\Module;
 use Yii;
+use yii\web\UnauthorizedHttpException;
 
 class IndexAction extends BaseAction
 {
+    public function beforeRun()
+    {
+        if(!$this->controller->rbacManager->haveEditorPermissions()){
+            throw new UnauthorizedHttpException(Module::t('blog', 'Unauthorized Access'));
+        }
+        return parent::beforeRun();
+    }
+
     public function run()
     {
         $categories = Category::find()->all();
