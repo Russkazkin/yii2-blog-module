f088a8d6971883aa7344f515de01224654376730
skazkin <russkazkin@gmail.com>
2020-01-07 08:44:21 +0300

    Close categories update and view for users

--- controllers/actions/category/UpdateAction.php
+++ controllers/actions/category/UpdateAction.php
@@ -6,10 +6,20 @@ namespace app\modules\blog\controllers\actions\category;
 
 use app\modules\blog\controllers\actions\BaseAction;
 use app\modules\blog\models\Category;
+use app\modules\blog\Module;
 use Yii;
+use yii\web\UnauthorizedHttpException;
 
 class UpdateAction extends BaseAction
 {
+    public function beforeRun()
+    {
+        if(!$this->controller->rbacManager->haveEditorPermissions()){
+            throw new UnauthorizedHttpException(Module::t('blog', 'Unauthorized Access'));
+        }
+        return parent::beforeRun();
+    }
+
     public function run($id)
     {
         /* @var $model Category */
--- controllers/actions/category/ViewAction.php
+++ controllers/actions/category/ViewAction.php
@@ -5,9 +5,19 @@ namespace app\modules\blog\controllers\actions\category;
 
 
 use app\modules\blog\controllers\actions\BaseAction;
+use app\modules\blog\Module;
+use yii\web\UnauthorizedHttpException;
 
 class ViewAction extends BaseAction
 {
+    public function beforeRun()
+    {
+        if(!$this->controller->rbacManager->haveEditorPermissions()){
+            throw new UnauthorizedHttpException(Module::t('blog', 'Unauthorized Access'));
+        }
+        return parent::beforeRun();
+    }
+
     public function run($id)
     {
         return $this->controller->render('view', [
