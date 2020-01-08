ccdea5e9add0161008568274287f558b532b67a8
skazkin <russkazkin@gmail.com>
2020-01-08 13:43:13 +0300

    Add new tags list

--- views/tag/index.php
+++ views/tag/index.php
@@ -8,9 +8,12 @@ use app\modules\blog\Module;
 use yii\helpers\Html;
 use yii\grid\GridView;
 
-/* @var $this yii\web\View */
-/* @var $searchModel app\modules\blog\models\search\TagSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+/**
+ * @var $this yii\web\View
+ * @var $tags \app\modules\blog\models\Tag[]
+ * @var $rbacManager \app\modules\auth\components\RbacComponent
+ */
+
 
 $this->title = Yii::t('blog', 'Tags');
 $this->params['breadcrumbs'][] = $this->title;
@@ -29,4 +32,47 @@ FormAdvancedAsset::register($this);
            data-overlayColor="#36404a"><?= Module::t('blog', 'Create Category')?>
         </a>
     </p>
+    <div class="card-box">
+        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap">
+            <thead>
+            <tr>
+                <th><?=Module::t('blog', 'Name')?></th>
+                <th><?=Module::t('blog', 'Actions')?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($tags as $tag):?>
+                <tr class="status-<?= $tag->status; ?>">
+                    <td><?= $tag->title; ?></td>
+                    <td>
+                        <?= Html::a('<i class="mdi mdi-note-text"></i>',
+                            ['view', 'id' => $tag->id],
+                            ['title' => 'View']) ?>
+                        <?= Html::a('<i class="mdi mdi-pencil-outline"></i>',
+                            ['update', 'id' => $tag->id],
+                            ['title' => 'Update']) ?>
+                        <?php if($tag->status == $tag::STATUS_ACTIVE):?>
+                            <?= Html::a('<i class="mdi mdi-eye-off"></i>',
+                                ['soft-delete', 'id' => $tag->id],
+                                ['title' => 'Hide']) ?>
+                        <?php elseif ($tag->status == $tag::STATUS_DELETED): ?>
+                            <?= Html::a('<i class="mdi mdi-eye"></i>',
+                                ['restore', 'id' => $tag->id],
+                                ['title' => 'Restore']) ?>
+                        <?php endif; ?>
+                        <?php if ($rbacManager->haveAdminPermissions()):?>
+                            <?= Html::a('<i class="mdi mdi-delete"></i>',
+                                ['delete', 'id' => $tag->id],
+                                [
+                                    'data' => ['method' => 'post', 'id' => $tag->id],
+                                    'title' => 'Delete',
+                                    'class' => 'article-list-delete'
+                                ]) ?>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
