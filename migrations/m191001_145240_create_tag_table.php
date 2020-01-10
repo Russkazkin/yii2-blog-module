b90725a17a0151aabdaeea1d26b4f0ddd86d95c4
Ruslan Skazkopodatelev <russkazkin@gmail.com>
2020-01-10 11:31:22 +0300

    Add title length to the tag migration

--- migrations/m191001_145240_create_tag_table.php
+++ migrations/m191001_145240_create_tag_table.php
@@ -20,7 +20,7 @@ class m191001_145240_create_tag_table extends Migration
         }
         $this->createTable('blog_tag', [
             'id' => $this->primaryKey(),
-            'title' => $this->string()->notNull(),
+            'title' => $this->string(32)->notNull(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer(11),
             'updated_at' => $this->integer(11),
