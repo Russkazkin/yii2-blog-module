986a69e7b71e56f64c051424e66f8c97502b00c5
skazkin <russkazkin@gmail.com>
2020-01-11 11:24:44 +0300

    Add ajax validation for category create in index view

--- controllers/actions/category/IndexAction.php
+++ controllers/actions/category/IndexAction.php
@@ -8,7 +8,9 @@ use app\modules\blog\controllers\actions\BaseAction;
 use app\modules\blog\models\Category;
 use app\modules\blog\Module;
 use Yii;
+use yii\web\Response;
 use yii\web\UnauthorizedHttpException;
+use yii\widgets\ActiveForm;
 
 class IndexAction extends BaseAction
 {
@@ -25,9 +27,15 @@ class IndexAction extends BaseAction
         $categories = Category::find()->all();
         $model = new Category();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            Yii::$app->session->setFlash('success', Module::t('blog', 'Category creation successful'));
-            return $this->controller->redirect(['view', 'id' => $model->id]);
+        if ($model->load(Yii::$app->request->post())) {
+            if (Yii::$app->request->isAjax) {
+                Yii::$app->response->format = Response::FORMAT_JSON;
+                return ActiveForm::validate($model);
+            }
+            if ($model->save()) {
+                Yii::$app->session->setFlash('success', Module::t('blog', 'Category creation successful'));
+                return $this->controller->redirect(['view', 'id' => $model->id]);
+            }
         }
 
         return $this->controller->render('index', [
