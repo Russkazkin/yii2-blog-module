d61caa67ae60969c41406a73099865418761e9b3
skazkin <russkazkin@gmail.com>
2020-01-20 16:49:21 +0300

    Regenerate BaseComment model

--- models/base/BaseComment.php
+++ models/base/BaseComment.php
@@ -2,25 +2,24 @@
 
 namespace app\modules\blog\models\base;
 
-use app\modules\blog\models\Article;
 use Yii;
-use yii\db\ActiveQuery;
-use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "blog_comment".
  *
  * @property int $id
  * @property string $text
- * @property int $user_id
+ * @property int|null $user_id
+ * @property int|null $parent_id
  * @property int $article_id
  * @property int $status
- * @property int $created_at
- * @property int $updated_at
+ * @property int|null $created_at
+ * @property int|null $updated_at
  *
- * @property Article $article
+ * @property BlogArticle $article
+ * @property AuthUser $user
  */
-class BaseComment extends ActiveRecord
+class BaseComment extends \yii\db\ActiveRecord
 {
     /**
      * {@inheritdoc}
@@ -36,10 +35,11 @@ class BaseComment extends ActiveRecord
     public function rules()
     {
         return [
-            [['text', 'user_id', 'article_id'], 'required'],
+            [['text', 'article_id'], 'required'],
             [['text'], 'string'],
-            [['user_id', 'article_id', 'status', 'created_at', 'updated_at'], 'integer'],
-            [['article_id'], 'exist', 'skipOnError' => true, 'targetClass' => Article::class, 'targetAttribute' => ['article_id' => 'id']],
+            [['user_id', 'parent_id', 'article_id', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['article_id'], 'exist', 'skipOnError' => true, 'targetClass' => BlogArticle::className(), 'targetAttribute' => ['article_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => AuthUser::className(), 'targetAttribute' => ['user_id' => 'id']],
         ];
     }
 
@@ -52,6 +52,7 @@ class BaseComment extends ActiveRecord
             'id' => Yii::t('blog', 'ID'),
             'text' => Yii::t('blog', 'Text'),
             'user_id' => Yii::t('blog', 'User ID'),
+            'parent_id' => Yii::t('blog', 'Parent ID'),
             'article_id' => Yii::t('blog', 'Article ID'),
             'status' => Yii::t('blog', 'Status'),
             'created_at' => Yii::t('blog', 'Created At'),
@@ -60,10 +61,18 @@ class BaseComment extends ActiveRecord
     }
 
     /**
-     * @return ActiveQuery
+     * @return \yii\db\ActiveQuery
      */
     public function getArticle()
     {
-        return $this->hasOne(Article::class, ['id' => 'article_id']);
+        return $this->hasOne(BlogArticle::className(), ['id' => 'article_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(AuthUser::className(), ['id' => 'user_id']);
     }
 }
