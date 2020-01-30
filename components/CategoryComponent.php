0d6c23289b8a48209be50ff87b5b0ecff35ba28f
skazkin <russkazkin@gmail.com>
2020-01-30 08:57:36 +0300

    Complete new method for category nav item

--- components/CategoryComponent.php
+++ components/CategoryComponent.php
@@ -8,6 +8,7 @@ use app\modules\blog\models\Article;
 use app\modules\blog\models\Category;
 use yii\base\BaseObject;
 use yii\db\ActiveQuery;
+use yii\helpers\ArrayHelper;
 
 class CategoryComponent extends BaseObject
 {
@@ -17,6 +18,19 @@ class CategoryComponent extends BaseObject
             /* @var $query ActiveQuery */
             $query->andWhere(['status' => Category::STATUS_ACTIVE]);
         }])->groupBy('category_id')->all();
-        return $articles;
+        $categories = Category::find()
+            ->where(['id' => ArrayHelper::getColumn($articles, 'category_id')])
+            ->with('articles')
+            ->all();
+        $arr = ArrayHelper::map($categories, 'id', 'title');
+        $items = [];
+        foreach ($arr as $id => $title) {
+            $items[] = [
+                'label' => $title,
+                'url' => '/blog/archive?id=' . $id,
+                'class' => 'nav-link',
+            ];
+        }
+        return $items;
     }
 }
